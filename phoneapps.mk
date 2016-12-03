f3290a90d5a10be212dfe41fa9f2823d36d53dbd
nian0114 <268078545@qq.com>
2016-12-03 10:53:50 +0800

    add makefile

---
+++ phoneapps.mk
@@ -0,0 +1,52 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+Bluetooth \
+BluetoothExt \
+CellBroadcastReceiver \
+CertInstaller \
+DeviceInfo \
+DocumentsUI \
+Ds2 \
+Ds2UI \
+FingerprintServiceExtension \
+HTMLViewer \
+KeyChain \
+MdtpService \
+NfcNci \
+PacProcessor \
+PhotoTable \
+PicoTts \
+PrintSpooler \
+QtiTelephonyService \
+SmartBar \
+Stk \
+TelephonyProvider \
+TimeService \
+UserDictionaryProvider \
+WAPPushManager \
+fastdormancy \
+nubia_IRControl3 \
+nubia_IRtest3 \
+telresources
+
+private-phone-priv-apps := \
+AppSettingsInfoProvider \
+BackupRestoreConfirmation \
+Camera com.qualcomm.location \
+DefaultContainerService \
+ExternalStorageProvider \
+FusedLocation \
+InputDevices \
+MediaProvider \
+ProxyHandler \
+QtiTetherService \
+SelfRegister \
+SelfStartProvider \
+SettingsProvider \
+SharedStorageBackup \
+Shell \
+ZNubiaSensor \
+com.qualcomm.location \
+qcrilmsgtunnel
