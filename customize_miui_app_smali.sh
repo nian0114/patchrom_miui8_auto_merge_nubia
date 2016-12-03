4b41cb5bcb6f8d63c84e5f9ddf78847762ae7ce3
nian0114 <268078545@qq.com>
2016-12-04 03:28:16 +0800

    add function

--- customize_miui_app_smali.sh
+++ customize_miui_app_smali.sh
@@ -5,6 +5,20 @@ GIT_APPLY=$PORT_ROOT/tools/git.apply
 
 curdir=`pwd`
 
+function applyPatch () {
+    for patch in `find $1 -name "*.patch"`
+    do
+        cd out
+        $GIT_APPLY ../$patch
+        cd ..
+        for rej in `find $2 -name *.rej`
+        do
+            echo "Patch $patch fail"
+            exit 1
+        done
+    done
+}
+
 function appendSmaliPart() {
   for file in `find $1 -name *.part`
   do
